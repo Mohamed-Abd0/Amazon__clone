7a96394cc0c44327c7367b3ff37e9e4c4400c409
afraa <af.h.1@hotmail.com>
2023-01-14 18:23:34 +0200

    merge home-page

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,47 +1,46 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import { Box } from '@mui/material';
-import {CartMenuSection} from './';
-import { overlayfunc } from '../Store/product_slice/ModalSlice';
+import { Box } from "@mui/material";
+import { CartMenuSection } from "./";
+import { overlayfunc } from "../Store/product_slice/ModalSlice";
 
 const Modal = () => {
-
-  const {overlayState} = useSelector(({ModalSlice}) => ModalSlice)
+  const { overlayState } = useSelector(({ ModalSlice }) => ModalSlice);
 
   const [viewState, setViewState] = useState(overlayState);
 
   const action = useDispatch();
 
   const ParentStyless = {
-    position : "fixed",
+    position: "fixed",
     top: 0,
-    left :0,
-    width : "100%",
+    left: 0,
+    width: "100%",
     height: "100vh",
-    backgroundColor : "rgba(0, 0, 0, 0.5)",
-    zIndex : 10
-  }
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    zIndex: 10,
+  };
 
-  const Parent = (
-    overlayState && <Box sx={ParentStyless} onClick={() => action(overlayfunc(false))}>
+  const Parent = overlayState && (
+    <Box sx={ParentStyless} onClick={() => action(overlayfunc(false))}>
       <CartMenuSection />
     </Box>
-  )
+  );
 
   useEffect(() => {
     const root = document.getElementById("modal").previousElementSibling;
     if (overlayState) {
       root.style.overflow = "hidden";
-      root.style.height = "100vh"
+      root.style.height = "100vh";
     } else {
-      root.style.overflow = "auto";
-      root.style.height = "auto"
+      // root.style.overflow = "auto";
+      // root.style.height = "auto"
     }
-  }, [overlayState])
+  }, [overlayState]);
 
-  return createPortal(Parent, document.getElementById("modal"))
-}
+  return createPortal(Parent, document.getElementById("modal"));
+};
 
-export default Modal
+export default Modal;
