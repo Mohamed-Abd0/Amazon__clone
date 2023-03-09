4ed0aadfaeb234f40f34ce9c0c7aca5713142aec
Mohmed_Yousry <mohmedahmed32@gmail.com>
2023-03-09 22:19:34 +0200

    create add product page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "../src/Pages/Home";
+import Layout from "../src/Components/Layout/Layout";
+import Cart from "../src/Pages/Cart";
+import LogIn from "../src/Pages/LogIn";
+import Payment from "./Pages/Payment";
+import NotFound from "../src/Pages/NotFound";
+import Product from "../src/Pages/Product";
+import { AddProduct } from "./Pages/AddProduct";
+import { AddCategory } from "./Pages/AddCategory";
+// AddCategory
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     errorElement: <NotFound />,
+//     children: [
+//       { index: true, element: <Home /> },
+//       { path: "/product/:productId", element: <Product /> },
+//       { path: "/cart", element: <Cart /> },
+//       { path: "/login", element: <LogIn /> },
+//       { path: "/payment", element: <Payment /> },
+//       // { path: "/addProduct", element: <Payment /> },
+//     ],
+//   },
 
-import './App.css';
-import { createBrowserRouter, createRoutesFromElements , Route , RouterProvider } from 'react-router-dom';
-import Home from '../src/Pages/Home';
-import Layout from '../src/Components/Layout/Layout';
-import Cart from '../src/Pages/Cart';
-import LogIn from '../src/Pages/LogIn';
-import Payment from './Pages/Payment';
-import NotFound from '../src/Pages/NotFound';
-import Product from '../src/Pages/Product';
+//   { path: "admin", children: [{ index: true, element: <AddProduct /> }] },
+// ]);
 
-
-
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Layout />} >
-    <Route index element={<Home />} />
-    <Route path="/product/:productId" element={<Product />} />
-    <Route path="/cart" element={<Cart />} />
-    <Route path= "/login" element={<LogIn />} />
-    <Route path= "/payment" element={<Payment />} />
-    <Route path= "*" element={<NotFound />} />
-  </Route>
-));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/product/:productId" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/addProduct" element={<AddProduct />} />
+      <Route path="/addCategory" element={<AddCategory />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -6,7 +6,17 @@ import lengRedusers from "./nav_slice/lengRedusers";
 import cartItemNumber from "./nav_slice/cartRedusers";
 import nameUserSlice from "./nav_slice/nameRedusers";
 import chechout from "./checkout_slice/checkoutSlice";
+import userSlice from "./nav_slice/userSlice";
 
 export default configureStore({
-  reducer: {ModalSlice, ProductSlice, CartSlice , leng : lengRedusers , chechout , cartItemNumber , nameUserSlice },
+  reducer: {
+    ModalSlice,
+    ProductSlice,
+    CartSlice,
+    leng: lengRedusers,
+    chechout,
+    cartItemNumber,
+    nameUserSlice,
+    user: userSlice,
+  },
 });
